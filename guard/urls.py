2fc044840cca776b2896dc6f23e7f60492e55e5d
syedusamarehman <mohammadusamaz11@gmail.com>
2024-07-18 21:31:45 +0500

    login updated

--- guard/urls.py
+++ guard/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("login/", views.login_view, name="login"),
+    path("/", views.login_view, name="login"),
     path('dashboard/', views.dashboard, name='dashboard'),
     path("logout/", views.logout_user, name="logout_user"),
     path('getdata/<int:pk>/',views.get_data,name="getdata"),
